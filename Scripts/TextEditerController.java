d7077967e8bbe2618cc3bbda959afa3389504354
nogami <g2053930@cc.kyoto-su.ac.jp>
2023-09-21 15:57:44 +0900

    [ADD]Action系のライブラリを追加

--- Scripts/TextEditerController.java
+++ Scripts/TextEditerController.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class TextEditerController extends Object implements ActionListener{
 
 	private StringConversionDataModel stringConversionDataModel;
