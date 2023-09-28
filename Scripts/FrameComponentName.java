a83bcaf71361ca61999a1a92e45f05f3d82eedac
nogami <g2053930@cc.kyoto-su.ac.jp>
2023-09-28 17:33:54 +0900

    [ADD]setting画面のボタンの名前を追加

--- Scripts/FrameComponentName.java
+++ Scripts/FrameComponentName.java
@@ -8,15 +8,15 @@ public class FrameComponentName {
 
 	public static final String FILE_SAVE_AS = "Save as";
 
-	public static final String IMPORT_BUTTON = "";
+	public static final String IMPORT_BUTTON = "Import";
 
-	public static final String EXPORT_BUTTON = "";
+	public static final String EXPORT_BUTTON = "Export";
 
-	public static final String ADD_STRING_PANEL_BUTTON = "";
+	public static final String ADD_STRING_PANEL_BUTTON = "Add group";
 
-	public static final String EXIT_SETTING_BUTTON = "";
+	public static final String EXIT_SETTING_BUTTON = "Save and exit";
 
-	public static final String SAVE_SETTING_BUTTON = "";
+	public static final String SAVE_SETTING_BUTTON = "Save";
 
 	public static final String TEXTEDITER = "";
 
