7b2aececc568d55c59326b224b267b2f19e4bdae
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2023-10-08 22:42:24 +0900

    [ADD]上書きするファイルにテキストエディタ―内の文字列で上書きするメソッド追加

--- Scripts/FileSaveModel.java
+++ Scripts/FileSaveModel.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileSaveModel extends FileBase {
 
 	public FileSaveModel() {
 
+	}
+
+	public void saveAsFile(File aSaveAsFile, String aTextEditerAreaString) throws IOException{
+		FileWriter saveAsFileWriter = new FileWriter(aSaveAsFile);
+		
+		saveAsFileWriter.write(aTextEditerAreaString);
+
+		saveAsFileWriter.close();
 	}
 
 }
