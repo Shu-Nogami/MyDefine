1f4eb20fff0f1f50b3117f274052a6feb4a2fc80
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2023-10-10 22:57:39 +0900

    [ADD]ユーザー設定のファイルを読込み、それぞれをlistに格納するメソッドを追加

--- Scripts/ConversionDataFileRoadModel.java
+++ Scripts/ConversionDataFileRoadModel.java
@@ -1,23 +1,71 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ConversionDataFileRoadModel extends FileBase {
 
+	private final String LINE_SEPARATOR = System.getProperty("line.separator");
+
+	private final String BEFORE_CONVERSION_STRING = "Before:";
+
+	private final String AFTER_CONVERSION_STRING = "After:";
+
 	public ConversionDataFileRoadModel() {
 
 	}
 
-	public List getConversionData(File aConversionDataFile, List aStringConversionDataMap) throws FileNotFoundException{
+	public List<Map<String, String>> getConversionData(File aConversionDataFile, List<Map<String, String>> aStringConversionDataMap) throws IOException{
 		Map<String, String> conversionDataMap = new HashMap<>();
 		BufferedReader conversionDataFileReader = new BufferedReader(new FileReader(aConversionDataFile));
+		String readLine = conversionDataFileReader.readLine();
 		StringBuilder conversionBeforeDataBuilder = new StringBuilder();
 		StringBuilder conversionAfterDataBuilder = new StringBuilder();
+		boolean isBeforeConversionData = true;
+
+		while(readLine != null){
+			if(readLine.equals(this.BEFORE_CONVERSION_STRING)){
+				if(!isBeforeConversionData){
+					System.out.println(conversionBeforeDataBuilder);
+					conversionDataMap.put(conversionBeforeDataBuilder.toString(), conversionAfterDataBuilder.toString());
+					conversionBeforeDataBuilder = this.resetStringBuilder(conversionBeforeDataBuilder);
+					conversionAfterDataBuilder = this.resetStringBuilder(conversionAfterDataBuilder);
+				}
+				isBeforeConversionData = true;
+				readLine = conversionDataFileReader.readLine();
+				continue;
+			}
+			if(readLine.equals(this.AFTER_CONVERSION_STRING)){
+				isBeforeConversionData = false;
+				readLine = conversionDataFileReader.readLine();
+				continue;
+			}
+			if(isBeforeConversionData){
+				this.setReadStringBuilder(readLine, conversionBeforeDataBuilder);
+			}
+			if(!isBeforeConversionData){
+				this.setReadStringBuilder(readLine, conversionAfterDataBuilder);
+			}
+			readLine = conversionDataFileReader.readLine();
+		}
+
+		conversionDataFileReader.close();
+		aStringConversionDataMap.add(conversionDataMap);
 
 		return aStringConversionDataMap;
+	}
+
+	private StringBuilder setReadStringBuilder(String aReadLine, StringBuilder aConversionDataBuilder){
+		aConversionDataBuilder.append(aReadLine);
+		aConversionDataBuilder.append(this.LINE_SEPARATOR);
+		return aConversionDataBuilder;
+	}
+
+	private StringBuilder resetStringBuilder(StringBuilder aConversionDataBuilder){
+		aConversionDataBuilder.delete(0, aConversionDataBuilder.length());
+		return aConversionDataBuilder;
 	}
 }
