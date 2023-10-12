9f53031d13ebfa4d9dca17e959bd5de38c61725a
野上柊 <g2053930@cc.kyoto-su.ac.jp>
2023-10-12 17:02:48 +0900

    [ADD]ラジオボタンの名前を追加

--- Scripts/FrameComponentName.java
+++ Scripts/FrameComponentName.java
@@ -30,4 +30,24 @@ public class FrameComponentName {
 
 	public static final String EDIT_MENU = "";
 
+	public static final String CONVERSION_FIRST_MAP = "first conversion map";
+
+	public static final String CONVERSION_SECOND_MAP = "second conversion map";
+
+	public static final String CONVERSION_THIRD_MAP = "third conversion map";
+
+	public static final String CONVERSION_FOURTH_MAP = "fourth conversion map";
+
+	public static final String CONVERSION_FIFTH_MAP = "fifth conversion map";
+
+	public static final String CONVERSION_SIXTH_MAP = "sixth conversion map";
+
+	public static final String CONVERSION_SEVENTH_MAP = "seventh conversion map";
+
+	public static final String CONVERSION_EIGHTH_MAP = "eighth conversion map";
+
+	public static final String CONVERSION_NINTH_MAP = "ninth conversion map";
+
+	public static final String CONVERSION_TENTH_MAP = "tenth conversion map";
+
 }
