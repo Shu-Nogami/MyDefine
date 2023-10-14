590347284fa90485eff43f71d0bd39f3f0096b14
野上柊 <g2053930@cc.kyoto-su.ac.jp>
2023-10-14 20:21:56 +0900

    [ADD]変換ファイルで使用する定数を設定するファイルを追加

---
+++ Scripts/ConversionDataFileConstant.java
@@ -0,0 +1,14 @@
+public class ConversionDataFileConstant{
+	public final static String CONVERSION_DATA_FILE_PATH = "./conversionData.txt";
+
+    public final static String BEFORE_CONVERSION_STRING = "Before:";
+
+	public final static String AFTER_CONVERSION_STRING = "After:";
+
+	public final static String END_CONVERSION_STRING = "End";
+
+    public final static String LINE_SEPARATOR = System.getProperty("line.separator");
+
+    public final static Integer CONVERSION_STRING_NUMBER = 10;
+
+}
