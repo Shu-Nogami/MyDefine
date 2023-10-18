a7d8eb17a163dc9d9defe94a23058e9cd1b6d7b1
野上柊 <g2053930@cc.kyoto-su.ac.jp>
2023-10-19 00:45:42 +0900

    [FIX]アプリケーションの名前を修正

--- Scripts/MyDefinition.java
+++ Scripts/MyDefinition.java
@@ -1,4 +1,4 @@
-public class MyDefine extends Object {
+public class MyDefinition extends Object {
 
 	public static void main(String[] args) {
 		StringConversionDataModel stringConversionDataModel = new StringConversionDataModel();
